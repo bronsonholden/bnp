e58289f848d9672d25924dbc402fba004cc81be4
Bronson Holden <bronson@bronsonholden.com>
2025-04-27 16:28:50 -0500

    Add tags component

--- bnp/src/bnp/components/label.h
+++ bnp/src/bnp/components/label.h
@@ -1,11 +1,16 @@
 #pragma once
 
 #include <string>
+#include <unordered_set>
 
 namespace bnp {
 
 	struct Label {
 		std::string value;
+	};
+
+	struct Tags {
+		std::unordered_set<std::string> values;
 	};
 
 }
