2a21167d8f11a82f77d1064dc998c4f3c75a15f9
Bronson Holden <bronson@bronsonholden.com>
2025-05-26 09:35:44 -0500

    Formatting

--- bnp/resources/shaders/planet_fragment_shader.glsl
+++ bnp/resources/shaders/planet_fragment_shader.glsl
@@ -17,9 +17,8 @@ uniform vec3 coast_color;
 uniform vec3 mainland_color;
 uniform vec3 mountain_color;
 
-//	Classic Perlin 3D Noise 
-//	by Stefan Gustavson (https://github.com/stegu/webgl-noise)
-//
+// Classic Perlin 3D Noise
+// by Stefan Gustavson (https://github.com/stegu/webgl-noise)
 vec4 permute(vec4 x){return mod(((x*34.0)+1.0)*x, 289.0);}
 vec4 taylorInvSqrt(vec4 r){return 1.79284291400159 - 0.85373472095314 * r;}
 vec4 fade(vec4 t) {return t*t*t*(t*(t*6.0-15.0)+10.0);}
