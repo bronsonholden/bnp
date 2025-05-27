899a13f852cdcb7531eb423623db04bf2635ba1b
Bronson Holden <bronson@bronsonholden.com>
2025-05-26 19:41:02 -0500

    Add some game components

---
+++ bnp/src/bnp/components/game/extraction.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <bnp/components/inventory.h>
+
+#include <vector>
+
+namespace bnp {
+
+	// stores the resources that can be extracted from a celestial body
+	// (planet, moon, asteroid)
+	struct CelestialExtractionTable {
+		std::vector<Item::ID> entries;
+	};
+
+}
---
+++ bnp/src/bnp/components/game/universe.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string>
+
+namespace bnp {
+
+	struct Celestial {
+		std::string name;
+	};
+
+}
