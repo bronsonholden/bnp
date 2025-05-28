29bdfca83f8e61158d654f8308b8c2d46611c48a
Bronson Holden <bronson@bronsonholden.com>
2025-05-28 10:28:12 -0500

    Add game inventory prefabs

---
+++ bnp/src/bnp/game/prefabs/inventory.cpp
@@ -0,0 +1,20 @@
+#include <bnp/game/prefabs/inventory.h>
+#include <bnp/components/inventory.h>
+
+namespace bnp {
+	namespace Prefab {
+
+		Node Inventory::item_database(entt::registry& registry) {
+			Node node(registry);
+
+			node.add_component<ItemDatabase>(ItemDatabase{
+				{
+					{"Iron Ore"}
+				}
+				});
+
+			return node;
+		}
+
+	}
+}
---
+++ bnp/src/bnp/game/prefabs/inventory.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <bnp/core/node.hpp>
+#include <entt/entt.hpp>
+
+namespace bnp {
+	namespace Prefab {
+		namespace Inventory {
+
+			Node item_database(entt::registry& registry);
+
+		}
+	}
+}
