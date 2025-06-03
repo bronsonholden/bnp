6f1e2897ecea7fd1d0575b7d18eef4db2a1d9a36
Bronson Holden <bronson@bronsonholden.com>
2025-06-03 07:51:43 -0500

    Add state components

---
+++ bnp/src/bnp/components/state.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace bnp {
+
+// Must be duplicated when creating a new game (without this component)
+struct NewGameState {};
+
+// Entities that must be serialized to save files
+struct SaveState {};
+
+}
