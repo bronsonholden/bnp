4332252e6a9000379aa2590700a737a09fba6d1d
Bronson Holden <bronson@bronsonholden.com>
2025-06-05 13:43:37 -0500

    Add world space to UI space helper

--- bnp/src/bnp/helpers/ui_helper.cpp
+++ bnp/src/bnp/helpers/ui_helper.cpp
@@ -8,4 +8,14 @@ glm::vec4 ui_space_to_world_space(glm::mat4 projection, float screen_width, floa
 	return glm::inverse(projection) * glm::vec4(x, y, 0.0f, 1.0f);
 }
 
+glm::vec2 world_space_to_ui_space(glm::mat4 projection, float screen_width, float screen_height, float x, float y) {
+	glm::vec4 clip_space = projection * glm::vec4(x, y, 0.0f, 1.0f);
+	clip_space /= clip_space.w;
+
+	float ui_x = (clip_space.x + 1.0f) * 0.5f * screen_width;
+	float ui_y = (1.0f - clip_space.y) * 0.5f * screen_height;  // flip Y for top-left origin
+
+	return glm::vec2(ui_x, ui_y);
+}
+
 }
--- bnp/src/bnp/helpers/ui_helper.h
+++ bnp/src/bnp/helpers/ui_helper.h
@@ -5,5 +5,6 @@
 namespace bnp {
 
 glm::vec4 ui_space_to_world_space(glm::mat4 projection, float screen_width, float screen_height, float x, float y);
+glm::vec2 world_space_to_ui_space(glm::mat4 projection, float screen_width, float screen_height, float x, float y);
 
 }
