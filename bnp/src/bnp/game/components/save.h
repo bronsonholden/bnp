a82e3862451a8f54331627e0334ab092463b819f
Bronson Holden <bronson@bronsonholden.com>
2025-06-13 19:52:15 -0500

    Clean up test stuff

--- bnp/src/bnp/game/components/save.h
+++ bnp/src/bnp/game/components/save.h
@@ -11,26 +11,8 @@ namespace Component {
 struct SaveData {
 };
 
-struct FakeDataA {
-	int value;
-};
-
-struct FakeDataB {
-	float value;
-};
-
 template <typename S>
 void serialize(S& s, SaveData& _) {
-}
-
-template <typename S>
-void serialize(S& s, FakeDataA& a) {
-	s.value4b(a.value);
-}
-
-template <typename S>
-void serialize(S& s, FakeDataB& b) {
-	s.value4b(b.value);
 }
 
 }
--- bnp/src/bnp/game/ui/navigation_control.cpp
+++ bnp/src/bnp/game/ui/navigation_control.cpp
@@ -7,7 +7,6 @@
 #include <fstream>
 
 #include <bnp/helpers/filesystem_helper.h>
-#include <bnp/game/components/save.h>
 #include <bitsery/bitsery.h>
 #include <bitsery/adapter/stream.h>
 
@@ -22,45 +21,6 @@ void NavigationControl::render(entt::registry& registry) {
 		navigation_manager.hide_celestial_map(registry);
 		navigation_manager.hide_system_map(registry);
 		navigation_manager.show_galaxy_map(registry);
-	}
-
-	if (ImGui::Button("Template Save")) {
-		entt::entity test_entity = registry.create();
-		registry.emplace<Component::SaveData>(test_entity);
-		registry.emplace<Component::FakeDataA>(test_entity, 2);
-		registry.emplace<Component::FakeDataB>(test_entity, 3.1415f);
-
-		entt::entity test_entity2 = registry.create();
-		registry.emplace<Component::SaveData>(test_entity2);
-		registry.emplace<Component::FakeDataA>(test_entity2, 4);
-
-		{
-			std::ofstream os(data_dir() / "test_save.bin");
-			bitsery::Serializer<bitsery::OutputStreamAdapter> ser{ os };
-			bnp::serialize<decltype(ser), Component::SaveData, Component::FakeDataA, bnp::Optional<Component::FakeDataB>>(ser, registry, 1);
-
-			ser.adapter().flush();
-			registry.destroy(test_entity);
-			registry.destroy(test_entity2);
-		}
-
-	}
-
-	if (ImGui::Button("Template load")) {
-		{
-			std::ifstream is(data_dir() / "test_save.bin");
-			bitsery::Deserializer<bitsery::InputStreamAdapter> des{ is };
-			uint32_t version = bnp::deserialize<decltype(des), Component::SaveData, Component::FakeDataA, bnp::Optional<Component::FakeDataB>>(des, registry);
-
-			Log::info("version %d", version);
-
-			auto view = registry.view<Component::SaveData, Component::FakeDataA>();
-			for (auto entity : view) {
-				Log::info("FakeDataA: %d", registry.get<Component::FakeDataA>(entity).value);
-				if (registry.all_of<Component::FakeDataB>(entity))
-					Log::info("FakeDataB: %.5f", registry.get<Component::FakeDataB>(entity).value);
-			}
-		}
 	}
 }
 
